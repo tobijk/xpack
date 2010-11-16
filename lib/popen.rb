fec05609ad39788c47d238c79d02a4972c1bce7e
Tobias Koch <tobias.koch@gmail.com>
2010-11-17 00:21:51 +0200

    add custom Popen module
    * popen2, popen2 for full control over stdin, stdout, stderr
    * execute commands in a sanitized environment
    * return exit code of subprocess

---
+++ lib/popen.rb
@@ -0,0 +1,80 @@
+# -*- encoding: utf-8 -*-
+#
+# This file is part of the XPack Package Manager
+# Copyright (C) 2010, Tobias Koch <tobias.koch@gmail.com>
+#
+# XPack is licensed under the GNU General Public License, version 2. A copy of
+# the license text can be found in the file LICENSE included in the source distribution.
+#
+
+module Popen
+
+  def self.popen2(cmd, env = {})
+    stdin_r, stdin_w = IO.pipe
+    stdeo_r, stdeo_w = IO.pipe
+
+    pid = fork
+    if pid
+      stdin_r.close
+      stdeo_w.close
+
+      yield stdin_w, stdeo_r
+
+      Process.waitpid(pid, 0)
+      proc_stat = $?
+      return proc_stat.exitstatus
+    else
+      stdin_w.close
+      stdeo_r.close
+
+      $stdin.reopen(stdin_r)
+      $stdout.reopen(stdeo_w)
+      $stderr.reopen(stdeo_w)
+
+      update_env(env)
+      exec cmd
+    end
+  end
+
+  def self.popen3(cmd, env = {})
+    stdin_r,  stdin_w  = IO.pipe
+    stdout_r, stdout_w = IO.pipe
+    stderr_r, stderr_w = IO.pipe
+
+    pid = fork
+    if pid 
+      stdin_r.close
+      stdout_w.close
+      stderr_w.close
+
+      yield stdin_w, stdout_r, stderr_r
+
+      Process.waitpid(pid, 0)
+      proc_stat = $?
+      return proc_stat.exitstatus
+    else 
+      stdin_w.close
+      stdout_r.close
+      stderr_r.close
+
+      $stdin.reopen(stdin_r)
+      $stdout.reopen(stdout_w)
+      $stderr.reopen(stderr_w)
+
+      update_env(env)
+      exec cmd
+    end
+  end
+
+  private
+
+  def self.update_env(env)
+    ENV.delete_if do |key, value|
+      not ['PATH', 'USER', 'USERNAME'].include?(key)
+    end
+    env.each_pair do |key, value|
+      ENV[key] = value
+    end
+  end
+
+end
