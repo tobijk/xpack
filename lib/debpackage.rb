51018d6b0021329a23cbaac7dab7fbb4395c420f
Tobias Koch <tobias.koch@gmail.com>
2010-12-19 01:04:51 +0200

    incomplete implementation of DebianPackage
    * generate package meta data
    * determine machine architecture from uname

--- lib/debpackage.rb
+++ lib/debpackage.rb
@@ -11,8 +11,52 @@ require 'binpackage'
 
 class DebianPackage < BinaryPackage
 
-  def pack(base_dir, output_dir)
+  def do_pack
+    puts meta_data + "\n"
+  end
 
+  def meta_data
+    meta = String.new
+    meta += "Package: #{@name}\n"
+    meta += "Version: #{@version}\n"
+    meta += "Source: #{@source}\n"
+    meta += "Maintainer: #{@maintainer}\n"
+    meta += "Section: #{@section}\n"
+    meta += "Architecture: %s\n" % \
+      (@is_arch_indep == 'true' ? 'all' : DebianPackage.debian_architecture)
+
+    if not @requires.empty?
+      meta += "Depends: " + \
+      @requires.collect do |pkg|
+        name = pkg[0]
+        version = pkg[1] == '==' ? "= #{@version}" : pkg[1]
+        if version
+          "#{name} (#{version})"
+        else
+          name
+        end
+      end\
+      .join(', ')
+      meta += "\n"
+    end
+
+    return meta
+  end
+
+  def self.debian_architecture
+    errmsg = "failed to detect machine architecture."
+
+    march = `uname -m`.chomp
+    if $? != 0: raise StandardError errmsg end
+
+    case march
+      when /i.86/
+        'i386'
+      when /x86-64/
+        'amd64'
+      else
+        raise StandardError errmsg
+      end
   end
 
 end
