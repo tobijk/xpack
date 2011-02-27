3375aff956a84ef19b34e335f2c9175a23fa87fb
Tobias Koch <tobias.koch@gmail.com>
2011-02-27 16:37:00 +0200

    basic functionality for creating debian packages completed

--- lib/debianpackage.rb
+++ lib/debianpackage.rb
@@ -7,8 +7,10 @@
 # the license text can be found in the file LICENSE included in the source distribution.
 #
 
+require 'tmpdir'
 require 'binarypackage'
 require 'digest/md5'
+require 'libarchive_rs'
 
 class DebianPackage < BinaryPackage
 
@@ -17,8 +19,104 @@ class DebianPackage < BinaryPackage
       '_' + debian_architecture + '.deb'
     package_file_name = File.expand_path(@output_dir + '/' + package_file_name)
 
-    puts package_file_name
-    puts meta_data
+    Dir.mktmpdir do |tmpdir|
+      write_data_tar_gz(tmpdir + '/data.tar.gz')
+      write_control_tar_gz(tmpdir + '/control.tar.gz')
+      File.open(tmpdir + '/debian-binary', 'w+') do |f|
+        f.write(debian_binary)
+      end
+
+      ar = Archive.write_open_filename(package_file_name,
+        Archive::COMPRESSION_NONE, Archive::FORMAT_AR_SVR4) do |ar|
+
+        [ 'debian-binary', 'control.tar.gz',
+            'data.tar.gz' ].each do |entry_name|
+          ar.new_entry do |ar_entry|
+            real_path = tmpdir + '/' + entry_name
+            ar_entry.copy_stat(real_path)
+            ar_entry.mode = Archive::ENTRY_FILE | 0644
+            ar_entry.pathname = entry_name
+            ar_entry.uid = 0
+            ar_entry.gid = 0
+            ar_entry.uname = 'root'
+            ar_entry.gname = 'root'
+            ar.write_header(ar_entry)
+            File.open(real_path) { |fp| ar.write_data { fp.read(1024) } }
+          end
+        end
+
+      end
+    end
+  end
+
+  def write_control_tar_gz(path)
+
+    ar = Archive.write_open_filename(path, Archive::COMPRESSION_GZIP,
+      Archive::FORMAT_TAR_USTAR) do |ar|
+
+      contents = [
+        [ 'control', meta_data ],
+        [ 'md5sums', md5sums   ]
+      ]
+
+      contents.each do |entry_name, entry_content|
+        ar.new_entry do |ar_entry|
+          ar_entry.mode = Archive::ENTRY_FILE | 0644
+          ar_entry.pathname = entry_name
+          ar_entry.atime = Time.now.to_i
+          ar_entry.mtime = Time.now.to_i
+          ar_entry.uid = 0
+          ar_entry.gid = 0
+          ar_entry.uname = 'root'
+          ar_entry.gname = 'root'
+          ar_entry.size = entry_content.bytesize
+          ar.write_header(ar_entry)
+          ar.write_data(entry_content)
+        end
+      end
+
+    end
+  end
+
+  def write_data_tar_gz(path)
+
+    ar = Archive.write_open_filename(path, Archive::COMPRESSION_GZIP,
+      Archive::FORMAT_TAR_USTAR) do |ar|
+
+      @contents.each do |entry|
+        file_path  = entry[0]
+        file_type  = entry[1][BinaryPackage::FILE_TYPE ]
+        file_mode  = entry[1][BinaryPackage::FILE_PERMS]
+        file_mode  = file_mode.oct if file_mode.class == String
+        file_owner = entry[1][BinaryPackage::FILE_OWNER]
+        file_group = entry[1][BinaryPackage::FILE_GROUP]
+        real_path  = File.expand_path(@base_dir + '/' + file_path)
+ 
+        begin
+          ar.new_entry do |ar_entry|
+            if file_type == 'directory' && !File.exists?(real_path)
+              ar_entry.mode = Archive::ENTRY_DIRECTORY | 0755
+              ar_entry.atime = Time.now.to_i
+              ar_entry.mtime = Time.now.to_i
+            else
+              ar_entry.copy_stat(real_path)
+            end
+            ar_entry.mode = ar_entry.filetype | file_mode if file_mode
+            ar_entry.pathname = file_path
+            ar_entry.uname = file_owner ? file_owner : 'root'
+            ar_entry.gname = file_group ? file_group : 'root'
+            ar.write_header(ar_entry)
+
+            if ar_entry.file?
+              File.open(real_path) { |fp| ar.write_data { fp.read(1024) } }
+            end
+          end
+        rescue Exception => e
+          raise StandardError, "error packaging '#{file_path}': #{e.message}\n"
+        end
+      end
+
+    end
   end
 
   def debian_binary
