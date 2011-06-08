a37616233cc5f6324cba5d314dde1e076a32b46c
Tobias Koch <tobias@tobijk.de>
2011-06-09 01:04:08 +0300

    update header sections in source files
    * call XPack a Package Generator instead of Package Manager
    * update copyright notices

--- lib/packagecontrol.rb
+++ lib/packagecontrol.rb
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 #
-# This file is part of the XPack Package Manager
-# Copyright (C) 2010, Tobias Koch <tobias.koch@gmail.com>
+# This file is part of the XPack Package Generator
+# Copyright (C) 2010-2011, Tobias Koch <tobias.koch@gmail.com>
 #
 # XPack is licensed under the GNU General Public License, version 2. A copy of
 # the license text can be found in the file LICENSE included in the source distribution.
